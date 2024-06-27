1c2e357d4d094cda40f088adc6c33a2b9cff7a3c
noutcheu libert <libert.noutcheu@gmail.com>
2024-06-27 13:57:03 +0100

    init the project add exo 4 and 5

---
+++ exo4/CMakeFiles/exo4.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/noutcheu/Documents/Info Master 1/Compilation/TP/exo4/analyseur/LexicalAnalyzer.cpp" "/home/noutcheu/Documents/Info Master 1/Compilation/TP/exo4/CMakeFiles/exo4.dir/analyseur/LexicalAnalyzer.cpp.o"
+  "/home/noutcheu/Documents/Info Master 1/Compilation/TP/exo4/main.cpp" "/home/noutcheu/Documents/Info Master 1/Compilation/TP/exo4/CMakeFiles/exo4.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
